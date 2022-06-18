6327b19ba4cd5babfc58b74a62fa7ec3ab36ef39
Daniel C <68665806+dchavez3395@users.noreply.github.com>
2022-06-18 16:02:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const menu = document.querySelector('#menu-btn');
+const navbar = document.querySelector('.header .nav');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+ window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+
+
+};
